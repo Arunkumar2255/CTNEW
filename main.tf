4023e48a6b25f459575cbb711192005f3a3bbf81
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-06-05 11:11:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "The AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "The AWS Access key"
+  type        = string
+  sensitive   = true
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "cdn" {
   source = "terraform-aws-modules/cloudfront/aws"
 
